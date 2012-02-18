c41a2cf4764c9ca99dfa2cccd79e38dfb1d0b38a
Michal Bernhard <michal@bernhard.cz>
2012-02-18 18:23:30 +0100

    implementation of findNearestCasinos

--- api-client/src/main/java/cz/mapyhazardu/api/domain/Hour.java
+++ api-client/src/main/java/cz/mapyhazardu/api/domain/Hour.java
@@ -18,5 +18,29 @@ public class Hour {
 	public int value() {
 		return hour;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + hour;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hour other = (Hour) obj;
+		if (hour != other.hour)
+			return false;
+		return true;
+	}
+	
+	
 	
 }
