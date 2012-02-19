defb40ffa0435e61eb34aff78e2b70856833f371
Vlastimil Dolejs <vlasta.dolejs@gmail.com>
2012-02-19 13:39:13 +0100

    save new casino

--- android-app/src/cz/mapyhazardu/android/activity/LocationActivity.java
+++ android-app/src/cz/mapyhazardu/android/activity/LocationActivity.java
@@ -3,8 +3,15 @@ package cz.mapyhazardu.android.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.android.actionbarcompat.R;
+
+import cz.mapyhazardu.android.MapyHazarduServiceProvider;
+import cz.mapyhazardu.api.domain.Casino;
+import cz.mapyhazardu.api.domain.Owner;
+import cz.mapyhazardu.api.domain.Runner;
 
 public class LocationActivity extends Activity {
 
@@ -15,6 +22,36 @@ public class LocationActivity extends Activity {
 	}
 
 	public void sendFeedback(View button) {
+		EditText textName = (EditText) findViewById(R.id.location_name);
+		EditText textHours = (EditText) findViewById(R.id.location_hours);
+		EditText textOwner = (EditText) findViewById(R.id.location_owner);
+		EditText textProvider = (EditText) findViewById(R.id.location_provider);
+		
+		Casino casino = new Casino();
+		casino.setName(textName.getText().toString());
+		casino.setOpeningHoursAsText(textHours.getText().toString());
+		
+		String companyNumber = textOwner.getText().toString();
+		if (companyNumber != null) {
+			casino.setOwner(new Owner(companyNumber));
+		}
+		
+		companyNumber = textProvider.getText().toString();
+		if (companyNumber != null) {
+			casino.setRunner(new Runner(companyNumber));
+		}
+		
+		// FIXME: doplnit pozici, kterou si vybral
+//		casino.setPosition(position);
+		
+		try {
+			MapyHazarduServiceProvider.getService().saveCasino(casino);
+			
+			Toast.makeText(this, R.string.message_save_ok, Toast.LENGTH_SHORT).show();
+		} catch (Throwable e) {
+			Toast.makeText(this, R.string.message_save_ok, Toast.LENGTH_LONG).show();
+		}
+		
 		finish();
 	}
 
