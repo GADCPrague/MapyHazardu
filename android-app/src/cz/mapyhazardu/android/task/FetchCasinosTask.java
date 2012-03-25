8cb2f4ee5b24322ac3933a6f38e18686289c8e76
Michal Bernhard <michal@bernhard.cz>
2012-03-25 18:13:26 +0200

    fetching casinos informs user when api is not available for any reason - before this fix application just go down

--- android-app/src/cz/mapyhazardu/android/task/FetchCasinosTask.java
+++ android-app/src/cz/mapyhazardu/android/task/FetchCasinosTask.java
@@ -3,14 +3,17 @@
  */
 package cz.mapyhazardu.android.task;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.android.maps.MapView;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
 import cz.mapyhazardu.android.CasinoOverlay;
 import cz.mapyhazardu.android.MapyHazarduServiceProvider;
+import cz.mapyhazardu.android.R;
 import cz.mapyhazardu.api.MapyHazardu;
 import cz.mapyhazardu.api.domain.Casino;
 import cz.mapyhazardu.api.domain.GeographicCoordinate;
@@ -25,6 +28,7 @@ public class FetchCasinosTask extends AsyncTask<GeographicCoordinate, Integer, L
 	private final CasinoOverlay casinoOverlay;
 	private final MapView mapView;
 	private static final String TAG = "FetchCasinosTask";
+	private Toast errorToast;
 	
 	public FetchCasinosTask(CasinoOverlay casinoOverlay, MapView mapView) {
 		super();
@@ -37,7 +41,15 @@ public class FetchCasinosTask extends AsyncTask<GeographicCoordinate, Integer, L
 		GeographicCoordinate position = param[0];
 		
 		Log.i(TAG, "Loading casino list");
-		List<Casino> casinos = MapyHazarduServiceProvider.getService().findNearestCasinos(position);
+		MapyHazardu mapyHazarduApi = MapyHazarduServiceProvider.getService();
+		
+		List<Casino> casinos = new ArrayList<Casino>();
+		try {
+			casinos = mapyHazarduApi.findNearestCasinos(position);
+		} catch (Throwable ex) {
+			Log.e(FetchCasinosTask.class.getSimpleName(), "Error when fetching nearest casinos information from web service.", ex);
+			errorToast.show();
+		}
 		
 		return casinos;
 	}
@@ -51,5 +63,11 @@ public class FetchCasinosTask extends AsyncTask<GeographicCoordinate, Integer, L
 		}
 		
 		mapView.postInvalidate();
+	}
+	
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+		errorToast = Toast.makeText(mapView.getContext(), R.string.message_error_when_obtaining_data_from_api, 10000);
 	}
 }
